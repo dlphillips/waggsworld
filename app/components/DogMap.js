7147e2e8feb9ba46a4070e11dedb9c089bd72e9d
Dan Phillips <dlphillips@gmail.com>
2017-08-04 15:32:03 -0400

    add basic leaflet map component

--- app/components/DogMap.js
+++ app/components/DogMap.js
@@ -1,28 +1,36 @@
-// Include React as a dependency
-var React = require("react");
 
-// Include the Query and Results components
-var Query = require("./search/Query");
-var Results = require("./search/Results");
+import React from 'react';
+import { render } from 'react-dom';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
-// Include the helpers for making API calls
-var helpers = require("../utils/helpers");
+class DogMap extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      lat: 35.229241,
+      lng: -80.842167,
+      zoom: 10,
+    };
+  }
 
-// Create the Search component
-var DogMap = React.createClass({
-
-  // Render the component. Note how we deploy both the Query and the Results Components
-  render: function() {
-
+  render() {
+    const position = [this.state.lat, this.state.lng];
     return (
-      <div className="main-container">
-
-        {/* Note how we pass the setQuery function to enable Query to perform searches */}
-        <h1>DogMap Component</h1>
-      </div>
+      <Map center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={position}>
+          <Popup>
+            <span>A pretty CSS3 popup. <br/> Easily customizable.</span>
+          </Popup>
+        </Marker>
+      </Map>
     );
   }
-});
+}
 
-// Export the module back to the route
 module.exports = DogMap;
+
+
