c343e9ca3399c27989a1a49a545617e2365570e4
Phil <phillip.haberern5@gmail.com>
2017-08-08 21:53:26 -0400

    fixed events page

--- app/components/Children/event.js
+++ app/components/Children/event.js
@@ -1,25 +1,55 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import axios from "../../utils/axios";
+
 class Event extends React.Component{
 
+    constructor(){
+        super();
+        this.state = ({
+            name: '',
+            desc: '',
+            url: '',
+            imgUrl: ''
+        });
+    }
+
+    componentDidMount(){
+        console.log("componentDidMount fired inside event component");
+        this.getArticle();
+    }
+
+    getArticle(){
+        axios.getArticles().then((res) =>{
+            var i = Math.floor(Math.random()*50);
+            console.log(res.data.events[i]);
+            this.setState({
+                name: res.data.events[i].name.text,
+                desc: res.data.events[i].description.text,
+                url: res.data.events[i].url,
+                imgUrl: res.data.events[i].logo.original.url
+            });
+            console.log("setState has fired");
+        });
+    }
 
 	render(){
 		return(
 			<div className="col-sm-6 col-md-6">
             	<div className="single_news">
                 	<div className="img_news">
-                		<img src={"http://placehold.it/1280x675"} alt="Blog Image" />
+                		<img src={this.state.imgUrl} alt="Blog Image" />
                 	</div>
                     <div className="news">
                        	<div className="topic">
                            	<div className="title_topic">
-                                <h4><a href="#">Google could learn from rival Microsoft in EU's antitrust decision</a></h4>
+                                <h4><a href={this.state.url}>{this.state.name}</a></h4>
                                 <p className="date pull-left"><i className="fa fa-calendar color"></i> 26 feb 2017</p>
                                 <p className="author_name pull-left"><i className="fa fa-user color"></i> By Admin</p>
                             </div>
                             <div className="clearfix"></div>
-                            <p className="mrg_top20 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged...</p>
+                            <p className="mrg_top20 text-muted">{this.state.desc}</p>
                         </div>
 
                         <div className="topic_info mrg_top40 text-center">
@@ -37,12 +67,9 @@ class Event extends React.Component{
                     </div>
                 </div>
             </div>
-			)
+			);
 	}
 }
-
-
-});
 
 
 
